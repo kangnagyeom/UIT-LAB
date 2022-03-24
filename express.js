4b762f40e9cf749bf551a5df7e475d87316bf85e
nagyeom <sotkwjs@naver.com>
2022-03-24 17:08:13 +0900

    first commit

---
+++ express.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  res.render('Hello UIT');
+});
+
+app.get('/first', (req, res) => {
+    res.render('FirstPage');
+})
+
+app.get('/second', (req, res) => {
+    res.render('SecondPage');
+})
+
+app.get('/login', (req, res) => {
+    res.render('LoginPage.ejs');
+});
+
+app.get('/announcement', (req, res) => {
+  res.render('announcementPage');
+})
+
+app.get('introduction', (req, res) => {
+  res.render('introductionPage')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
